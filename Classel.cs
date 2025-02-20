17b5d0e0355002496c85490bcbbddbd0c5d91985
Sillas Endrigo <sillas.silva@aluno.senai.br>
2025-02-20 16:57:07 -0300

    feat: atualizando o projeto, criando a pasta Controller e configurando Cadastrar Genero e Listar Genero.

--- Classel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace api_filmes_senai
-{
-    public class Classel
-    {
-    }
-}
---
+++ Repositories/GeneroRepository.cs
@@ -0,0 +1,77 @@
+﻿using api_filmes_senai.Context;
+using api_filmes_senai.Domains;
+using api_filmes_senai.Interfaces;
+
+namespace api_filmes_senai.Repositories
+{
+    /// <summary>
+    ///     Classe que vai implementar a interface IGeneroRepository.
+    ///     Ou seja, vamos implementar os métodos, toda a lógica dos metodos.
+    /// </summary>
+    public class GeneroRepository : IGeneroRepository
+    {
+        /// <summary>
+        /// Variável privada e somente leitura que "guarda" os dados no contexto
+        /// </summary>
+        /// 
+        private readonly Filmes_Context _context;
+
+        /// <summary>
+        /// Construtor do repositório
+        /// Aqui, toda vez que o construtor for chamado,
+        /// os dados do contexto estarão disponíveis.
+        /// </summary>
+        /// <param name="contexto">Dados do contexto</param>
+
+        public GeneroRepository(Filmes_Context contexto)
+        {
+            _context = contexto; 
+        }
+
+        public void Atualizar(Guid id, Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Genero BuscarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Genero novoGenero)
+        {
+            try
+            {
+                //Adiciona um novo gênero na tabela Generos(BD)
+                _context.Genero.Add(novoGenero);
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Genero> Listar()
+        {
+            try
+            {
+                List<Genero> ListaGeneros = _context.Genero.ToList();
+
+                return ListaGeneros;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
